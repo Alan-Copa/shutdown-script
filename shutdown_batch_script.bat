8766dc60e03d4d224c046e24b4e148d56cbc245a
Alan Copa <Alan@COPACO.local>
2023-11-14 17:49:59 +0100

    quick fix

--- shutdown_batch_script.bat
+++ shutdown_batch_script.bat
@@ -1,1 +1,1 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Users\Alan\Desktop\sh-script\shutdown_pwrshll_script.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Users\Alan\Desktop\progetti_locali\shutdown-script\shutdown_pwrshll_script.ps1"
